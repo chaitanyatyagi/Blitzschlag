77ca762d7e4b48aeac62aeda2b3b5e48a9927a1a
Harshit <2021ucp1382@mnit.ac.in>
2023-01-06 01:40:41 +0530

    profile fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -13,29 +13,29 @@ export default function Profile() {
 	});
 
 	return (
-		<div className="body">
+		<div className="profile-body">
 			<Navbar />
-			<div className="container">
-				<div className="head-div">
-					<h1 className="heading">&lt; User Profile &gt;</h1>
+			<div className="profile-container">
+				<div className="profile-head-div">
+					<h1 className="profile-heading">&lt; User Profile &gt;</h1>
 				</div>
-				<div className="content">
-					<div className="user-side">
-						<div className="user-profile">
-							<div className="user-img"></div>
-							<div className="user-details">
-								<div className="user-name">{userInfo.fullName}</div>
-								<div className="user-contact">
-									<div className="details-holder">
-										<div className="user-icons-username"></div>
-										<h3 className="user-contact-details">
+				<div className="profile-content">
+					<div className="profile-user-side">
+						<div className="profile-user-profile">
+							<div className="profile-user-img"></div>
+							<div className="profile-user-details">
+								<div className="profile-user-name">{userInfo.fullName}</div>
+								<div className="profile-user-contact">
+									<div className="profile-details-holder">
+										<div className="profile-user-icons-username"></div>
+										<h3 className="profile-user-contact-details">
 											{" "}
 											: {userInfo.username}
 										</h3>
 									</div>
-									<div className="details-holder">
-										<div className="user-icons-email"></div>
-										<h3 className="user-contact-details">
+									<div className="profile-details-holder">
+										<div className="profile-user-icons-email"></div>
+										<h3 className="profile-user-contact-details">
 											{" "}
 											: {userInfo.email}
 										</h3>
@@ -43,12 +43,12 @@ export default function Profile() {
 								</div>
 							</div>
 						</div>
-						<div className="blitz-logo"></div>
+						<div className="profile-blitz-logo"></div>
 					</div>
-					<div className="separator"></div>
-					<div className="event-side">
-						<h1 className="event-head">Registered Events</h1>
-						<div className="event-list">{eventData}</div>
+					<div className="profile-separator"></div>
+					<div className="profile-event-side">
+						<h1 className="profile-event-head">Registered Events</h1>
+						<div className="profile-event-list">{eventData}</div>
 					</div>
 				</div>
 			</div>
